e013dbc097e36b6894465a220301ad8cb50df19a
Hurricane Cui <hurricane.cui@gmail.com>
2018-10-25 23:05:11 -0400

    0033 sol2 not ac

--- normal_things/0033_search_in_rotated_sorted_array.py
+++ normal_things/0033_search_in_rotated_sorted_array.py
@@ -3,6 +3,7 @@
 # no duplicate exists
 # binary search
 
+
 class Solution:
 
     # don't use this solution, its trash
@@ -38,7 +39,7 @@ class Solution:
             elif target < nums[left] and target < nums[mid] and target <= nums[right]:
                 if nums[left] < nums[mid]:
                     left = mid + 1
-                    mid = (left + right) // 2  
+                    mid = (left + right) // 2
                 else:
                     right = mid - 1
                     mid = (left + right) // 2
@@ -65,16 +66,45 @@ class Solution:
                 else:
                     left = mid + 1
                     mid = (left + right) // 2
+        return -1
+
+    def search(self, nums, target):
+        # still not accepted
+        # im enough with this idiot
+        if len(nums) == 0:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        mid = left + (right - left) // 2
+        while left <= right:
+            print("========================")
+            print("left\tmid\tright")
+            print("{}\t{}\t{}".format(nums[left], nums[mid], nums[right]))
+            if target == nums[mid]:
+                return mid
+            elif nums[mid] < nums[right]:  # right half sorted
+                if target > nums[mid] and target <= nums[right]:  # target may be on right side
+                    left = mid + 1
+                    mid = left + (right - left) // 2
+                else:  # target may be on left side
+                    right = mid - 1
+                    mid = left + (right - left) // 2
+            elif nums[mid] > nums[left]:  # left half sorted
+                if target < nums[mid] and target >= nums[left]:  # target may be on left side
+                    right = mid - 1
+                    mid = left + (right - left) // 2
+                else:
+                    left = mid + 1
+                    mid = left + (right - left) // 2
 
         return -1
 
 
-    def search(self, nums, target):
-        # passed solution
-        pass
-
-
 sol = Solution()
 nums = [3,1]
-target = 1
+target = 0
 print(sol.search(nums, target))
