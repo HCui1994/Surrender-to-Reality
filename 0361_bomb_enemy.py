6175fe8cba24a8abf37f8fcc9c4d2fae72e67014
HCui <hurricane.cui@gmail.com>
2018-11-19 17:14:34 -0500

    AC, dynamic programming

--- 0361_bomb_enemy.py
+++ 0361_bomb_enemy.py
@@ -15,9 +15,13 @@ E 0 W E
 Placing a bomb at (1,1) kills 3 enemies.
 """
 
-class Solution:
 
+class Solution:
     def bomb_enemy_dp(self, grid):
+        """
+        memo_top_left[i][j] 表示 grid[i - 1][j - 1] 左侧（横） 和 上侧（竖）分别有多少个可以炸到的 enemy
+        memo_bottom_right[i][j] 表示 grid[i][j] 的右侧（横）和下侧（竖）分别 。。。
+        """
         if not grid:
             return 0
         if not grid[0]:
@@ -30,10 +34,13 @@ class Solution:
             for j in range(n):
                 mj = j + 1
                 if grid[i][j] == "W":
+                    # 如果 此处为 Wall，则横竖均炸不到任何东西
                     memo_top_left[mi][mj] = [0, 0]
                 elif grid[i][j] == "E":
+                    # 如果此处为 Enemy，则比 memo_top_left[mi][mj - 1] 横向可以多炸到一个，比 memo_top_left[mi - 1][mj] 竖向可以多炸到一个
                     memo_top_left[mi][mj] = [memo_top_left[mi][mj - 1][0] + 1, memo_top_left[mi - 1][mj][1] + 1]
                 else:
+                    # 如果此处为 0，则横向能炸到的和 memo_top_left[mi][mj - 1] 相同，竖向和 memo_top_left[mi - 1][mj] 相同 
                     memo_top_left[mi][mj] = [memo_top_left[mi][mj - 1][0], memo_top_left[mi - 1][mj][1]]
         for i in range(m - 1, -1, -1):
             for j in range(n - 1, -1, -1):
