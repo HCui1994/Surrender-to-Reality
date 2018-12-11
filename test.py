2fcc3fb2a825f65197afe5b87e7fa9c3a9c6a38d
HCui <hurricane.cui@gmail.com>
2018-12-11 16:21:53 -0500

    update

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-a = [0] * 10
-a[5] = 999
-print(a)
+class Shit:
+    def __init__(self):
+        self.string = "this is the new shit"
+
+    def __access(self):
+        return self.string
+
+
+shit = Shit()
+shit.shit = "wtf"
+print(shit.__attr__)
