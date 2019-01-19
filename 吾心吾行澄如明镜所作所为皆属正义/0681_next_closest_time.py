076d5c7d080b53ff6261138f4763f41752ee8c7d
HCui <hurricane.cui@gmail.com>
2019-01-18 20:31:49 -0500

    ac

--- 吾心吾行澄如明镜所作所为皆属正义/0681_next_closest_time.py
+++ 吾心吾行澄如明镜所作所为皆属正义/0681_next_closest_time.py
@@ -17,3 +17,58 @@ Output: "22:22"
 Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22. 
 It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
 """
+
+
+class Solution(object):
+    def next_closest_time(self, time):
+        digits = set(list(time))
+        hour, minute = time.split(":")
+        hour, minute = int(hour), int(minute)
+        next_hour = prev_hour = hour
+        next_minute = prev_minute = minute
+        while True:
+            next_minute += 1
+            if next_minute == 60:
+                next_hour += 1
+                next_minute = 0
+            if next_hour == 24:
+                next_hour = 0
+            next_time = "{:02d}:{:02d}".format(next_hour, next_minute)
+            prev_minute -= 1
+            if prev_minute == -1:
+                prev_minute = 59
+                prev_hour -= 1
+            if prev_hour == -1:
+                prev_hour = 23
+            prev_time = "{:02d}:{:02d}".format(prev_hour, prev_minute)
+            is_next_found = True
+            for digit in next_time:
+                if digit not in digits:
+                    is_next_found = False
+                    break
+            if is_next_found:
+                return next_time
+            is_prev_found = True
+            for digit in prev_time:
+                if digit not in digits:
+                    is_prev_found = False
+            if is_prev_found:
+                return prev_time
+            print(prev_time, next_time)
+
+    def nextClosestTime(self, time):
+        cur = 60 * int(time[:2]) + int(time[3:])
+        # print(cur)
+        allowed = {int(x) for x in time if x != ':'}
+        while True:
+            cur = (cur + 1) % (24 * 60)
+            if all(digit in allowed for block in divmod(cur, 60) for digit in divmod(block, 10)):
+                return "{:02d}:{:02d}".format(*divmod(cur, 60))
+
+    def test(self):
+        time = "01:34"
+        print(self.next_closest_time(time))
+        print(self.nextClosestTime(time))
+
+
+Solution().test()
